fc9ca01bf357fb9c149d115c3468f6ef838e1223
Jacob Smith <jakegee@users.noreply.github.com>
2017-11-06 14:14:30 +0000

    First upload

---
+++ Ex4.java
@@ -0,0 +1,23 @@
+// In this exercise we're adding and multiplying fractions, the answers given are in decimal, but using the methods learned and applied in the previous exercises and reading material we're able to simply calculate and print to screen the answers.
+
+public class Ex4 
+	{
+	public static void main(String[] args) 
+	{
+	
+	double es; //e sum
+	double ds; //d sum
+	double ep; //e product
+	double dp; //d product
+
+	es = (1.0/2) + (1.0/3);
+	ds = (1.0/3) + (3.0/4);
+	ep = (1.0/2) * (2.0/3);
+	dp = (1.0/4) * (2.0/3);
+
+	System.out.println(es);
+	System.out.println(ds);
+	System.out.println(ep);
+	System.out.println(dp);
+	}
+}
