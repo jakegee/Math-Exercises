2c26742a5439188db792df52016c568a8a93982a
Jacob Smith <jakegee@users.noreply.github.com>
2017-11-06 14:10:56 +0000

    Revert "Area Computation"
    This reverts commit ef500340afaf7873abf0266974c6bc9d71cc9134.

--- Ex1.java
+++
@@ -1,9 +0,0 @@
-//This program simply calculates the area of a circle when r = 5
-
-public class Ex1 { 
-	public static void main(String[] args) {
-
-	double r = 5; // Aided from the Friday lab lecture
-	System.out.println(r*r*Math.PI);
-	}
-}
